6b2ed6455429c20c61f6796a571e35f2f7d01ae2
Sam Wyss <sjwyss@outlook.com>
2023-11-07 15:48:47 -0500

    removed old comment

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use fileman_rs::Config;
 fn main() {
 
     // create new config enum
-    // need to add error handling and use iterators here, see 289 in book for example
     let _config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Error in configuration: {err}");
         process::exit(1);
